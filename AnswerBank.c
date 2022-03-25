f71ca87936caff042a2f87bed6bb779128785eff
josephdprince <57415450+josephdprince@users.noreply.github.com>
2022-03-25 10:45:25 -0700

    Answer bank instructions

--- AnswerBank.c
+++ AnswerBank.c
@@ -3,12 +3,5 @@ struct Phrases {
 	int size;
 	};
 
-struct Phrases bank[7] = {
-	{"Raphtalia Best G", 15}, 
-	{"JOSHUA PRINCE", 13}, 
-	{"ALWAYS MEMPHIS NEVER JOSHUA", 27}, 
-	{"MAKO CHAN", 9},
-	{"JIMMYBOY3503", 12}, 
-	{"PRIN CEE", 8}, 
-	{"REM REM REM REM", 15} 
-	};
+// To update, increase array size and hardcode a Phrases struct 
+struct Phrases bank[0] = {};
